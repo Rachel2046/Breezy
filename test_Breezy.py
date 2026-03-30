36097a4e945f039809f2fb00192211ec3cc46de4
Rachel <ruiminpan@hotmail.com>
2026-03-30 21:12:02 +1100

    add test file and .circleci/config file

---
+++ test_Breezy.py
@@ -0,0 +1,8 @@
+# faux test file for Breezy, used for setting up CI in circleci
+import time
+
+
+def test_faux_Breezy():
+    print("Testing......")
+    time.sleep(3)  # Pauses the program for 3 seconds
+    print("Test complete")
